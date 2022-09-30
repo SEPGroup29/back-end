98811e5ae50d9feaeac0eb385dfce970b4581b78
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-09-30 11:42:00 +0530

    Show vehicle owners implemented

--- routes/api/vehicle_owner.js
+++ routes/api/vehicle_owner.js
@@ -6,5 +6,6 @@ router
     .post('/add-vehicle', vehicleOwnerController.addVehicle)
     .get('/show-vehicles', vehicleOwnerController.showVehicles)
     .delete('/delete-vehicle/:vehicle_id', vehicleOwnerController.deleteVehicle)
+    .get('/show-all-vehicle-owners', vehicleOwnerController.showAllVehicleOwners)
 
 module.exports = router;
