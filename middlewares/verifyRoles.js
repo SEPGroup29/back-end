21a6045e74c9422a64e3b124a46f77ac9f51d46e
Deshan Lakshitha <deshanlaskhithawickramasinghe@gmail.com>
2022-10-16 02:12:18 +0530

    JWT and roles verification middlewares

---
+++ middlewares/verifyRoles.js
@@ -0,0 +1,22 @@
+const verifyRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user_id) {
+            return res.status(401).json({ message: "User id is missing." });
+        }
+
+        const userRole = req.role
+        const isAllow = allowedRoles.includes(userRole)
+
+        console.log("allowedRoles", allowedRoles)
+        console.log("user role", userRole)
+
+
+        if (!isAllow) {
+            console.log("not allowed role")
+            return res.status(401).json({ message: "Unauthorized request user not allowed" });
+        }
+        next()
+    }
+}
+
+module.exports = verifyRoles;
