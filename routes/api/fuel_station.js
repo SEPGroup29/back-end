a78442bf3adf9a301a74e2c7f8ff08d57a61d139
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-10-30 16:29:03 +0530

    Case insensitive search for fuel stations

--- routes/api/fuel_station.js
+++ routes/api/fuel_station.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router
     .post('/add-fuel-station', fuelStationController.insertFuelStation)
-    .get('/show-all-fuel-stations', fuelStationController.showAllFuelStations)
+    .get('/show-all-fuel-stations/:search', fuelStationController.showAllFuelStations)
     .post('/update-stock', fuelStationController.updateStock)
     .get('/get-stock/:fs_id', fuelStationController.getStock)
 
