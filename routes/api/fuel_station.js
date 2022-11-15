5cc99be86cad225c9923f41de35de1be60b4d7f4
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-11-15 10:06:42 +0530

    Admin - Show fuel stations

--- routes/api/fuel_station.js
+++ routes/api/fuel_station.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router
     .post('/add-fuel-station', verifyRoles(process.env.FUEL_STATION_MANAGER), fuelStationController.insertFuelStation)
-    .get('/show-all-fuel-stations/:search', verifyRoles(process.env.VEHICLE_OWNER), fuelStationController.showAllFuelStations)
+    .get('/show-all-fuel-stations/:search', verifyRoles(process.env.ADMIN, process.env.VEHICLE_OWNER), fuelStationController.showAllFuelStations)
     .post('/update-stock', verifyRoles(process.env.FUEL_STATION_MANAGER), fuelStationController.updateStock)
     .get('/get-stock/:fs_id', verifyRoles(process.env.FUEL_STATION_MANAGER), fuelStationController.getStock)
     .get('/get-three-fuel-stations', verifyRoles(process.env.VEHICLE_OWNER), fuelStationController.getThreeFuelStations)
